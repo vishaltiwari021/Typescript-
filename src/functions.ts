45e657504039ceeb5a9c3251d47c3b522f434359
vishaltiwari021 <vishaltiwarivt021@gmail.com>
2026-01-10 16:41:27 +0530

    functions In TS

---
+++ src/functions.ts
@@ -0,0 +1,16 @@
+//Functions:-
+
+function makeChai (type:string,cup:number){
+    console.log(`Making${cup} cups of chai ${type}`);
+}
+makeChai("gingerchai",2)
+
+function orderChai(order:string){
+    if(!order) return null
+    else return order
+}
+
+function logger():void{
+    console.log(`money`);
+    
+}
